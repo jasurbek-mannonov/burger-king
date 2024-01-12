821da6be1098ec012ac081f5785fe537ffe00fc2
jasurbek-mannonov <jasurbekmannonov2020@gmail.com>
2024-01-12 22:53:58 +0500

    changes

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+ <div class="bg-red-900 h-[100vh]">
+    <Navbar/>
+ </div>
+</template>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="d-flex text-red-900 bg-white m-auto">
+       <div>
+            <h3 class="font-normal text-[45.221px]">Burger</h3>
+       </div>
+       <div>
+            <ul>
+                <li>Home</li>
+                <li>Burger</li>
+                <li>Map</li>
+                <li>About</li>
+            </ul>
+       </div>
+       <button class="bg-red-900 text-white">
+            Order
+       </button>
+    </div>
+</template>
